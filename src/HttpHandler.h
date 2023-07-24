ac5e62e2467fefdedd11785263f2957f11983d76
jslucas22 <jose.esbellute@gmail.com>
2023-07-24 11:11:09 -0300

    Create method prototypes

--- src/HttpHandler.h
+++ src/HttpHandler.h
@@ -1,13 +1,25 @@
-class HttpHander
+#ifndef HTTP_HANDLER_H
+#define HTTP_HANDLER_H
+
+#include <string>
+#include <boost/asio.hpp>
+
+class HttpHandler
 {
 public:
-    void handleRequest()
-    {
-    }
-
-    void handleResponse()
-    {
-    }
+    HttpHandler(boost::asio::ip::tcp::socket clientSocket);
+    void handle();
 
 private:
+    void handleRequest();
+    void handleResponse();
+
+    boost::asio::ip::tcp::socket clientSocket;
+    std::string requestData;
+    std::string responseData;
+
+    const std::string kOriginalResponseWebglVendor = "gl.getParameter(gl.VENDOR)";
+    const std::string kOriginalResponsewebglRenderer = "gl.getParameter(gl.RENDERER)";
 };
+
+#endif // HTTP_HANDLER_H
